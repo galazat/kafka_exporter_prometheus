a2d9774cb395c697fa0b0c7f2ee6c2110ee7b360
Azat Gallyamov <azatgallyamov@Azats-MacBook-Air.local>
2026-06-08 21:58:21 -0400

    [part-1] - base endpoints

--- main.go
+++ main.go
@@ -5,19 +5,37 @@ import (
 	"net/http"
 )
 
+const (
+	listenAddr = ":9308"
+	metricsPath = "/metrics"
+)
+
 func main() {
 	// Создаем свой ServeMux - явный и изолированный, а не используем глобальный. Пустой маршрутизатор
 	mux := http.NewServeMux()
 
 	// Регистрируем handler на корневой путь "/"
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// w.Write отправляет байты в тело ответа
 		w.Write([]byte("kafka_exporter is active\n"))
 	})
 
+	// эндпоинт /healthz - это проба, сервис живой. Например, k8s проверяет - жив ли процесс. Пока просто отвечаем "ok"
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("ok"))
+	})
+
+	// эндпоинт /metrics - здесь будут метрики. Пока заглушка в формате Prometheus.
+	mux.HandleFunc(metricsPath, func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; version=0.0.4")
+		w.Write([]byte("# HELP my_exporter_up Is the exporter running.\n"))
+		w.Write([]byte("# TYPE my_exporter_up gauge\n"))
+		w.Write([]byte("my_exporter_up 1\n"))
+	})
+
+
 	// Описываем сервер, слушаем на порту 9308, используем наш mux.
 	server := &http.Server{
-		Addr:    ":9308",
+		Addr:    listenAddr,
 		Handler: mux,
 	}
 
